8462e135a39bf4a36da995f82dc1da8fe0166d65
RAVIRAJ SOLANKI <74584237+RaviSolanki27@users.noreply.github.com>
2022-11-04 21:44:32 +0530

    Updated Loader file

--- src/Utils/Loader.tsx
+++ src/Utils/Loader.tsx
@@ -2,30 +2,32 @@ import "./common.css";
 
 export const Loader = () => {
   return (
-    <div className="main-div">
+    <div className="loader-outer">
       <span className="initial text-6xl">R</span>
-      <section className="container1">
-        <div className="outdiv1">
-          <div className="indiv1">
-            <span className="one h6 span1"></span>
-            <span className="two h3 span2"></span>
+      <div className="main-div">
+        <section className="container1">
+          <div className="outdiv1">
+            <div className="indiv1">
+              <span className="one h6 span1"></span>
+              <span className="two h3 span2"></span>
+            </div>
           </div>
-        </div>
 
-        <div className="outdiv2">
-          <div className="indiv2">
-            <span className="one h1 span3"></span>
-            <span className="two h4 span4"></span>
+          <div className="outdiv2">
+            <div className="indiv2">
+              <span className="one h1 span3"></span>
+              <span className="two h4 span4"></span>
+            </div>
           </div>
-        </div>
 
-        <div className="outdiv3">
-          <div className="indiv3">
-            <span className="one h5 span5"></span>
-            <span className="two h2 span6"></span>
+          <div className="outdiv3">
+            <div className="indiv3">
+              <span className="one h5 span5"></span>
+              <span className="two h2 span6"></span>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
